4ad56258198bf478c9458e998872901e2731ba10
jen-soft <jen-soft@users.noreply.github.com>
2019-04-30 05:31:21 -0400

    Update pydocker.py

--- pydocker.py
+++ pydocker.py
@@ -146,8 +146,10 @@ class DockerFile(object):
 
     # -------------------------------------------------------------------- #
 
-    def RUN(self, dst_path, content):
-        self.__setattr__('RUN', self.file(dst_path, content))
+    def RUN(self, *args):
+        for cmd in args:
+            self.__setattr__('RUN', cmd)
+        #
 
     def COPY(self, dst_path, content):
         self.__setattr__('COPY', self.file(dst_path, content))
